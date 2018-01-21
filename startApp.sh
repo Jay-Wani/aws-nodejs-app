ef1b4d834e74836a095bef7a31be431ba2673f61
Junaid Wani <w.m.junaid@gmail.com>
2018-01-21 22:08:32 +0530

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=isdevdb.cgalg1q7janj.us-east-1.rds.amazonaws.com
+export DB_PRD_HOST=isdevdb.us-east-1.rds.amazonaws.com
 export DB_PRD_USER=adminis
-export DB_PRD_PASS=Password1!
+export DB_PRD_PASS=Passw0rd
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
